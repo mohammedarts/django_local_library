175ad7bc91dcf0e6302a12a0790cecbcb0aa3146
Mohammed Abdulmahdi <goodlives627@gmail.com>
2023-02-17 10:14:08 -0500

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['web-production-3640.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.railway.com', '127.0.0.1']
 
 
 # Application definition
